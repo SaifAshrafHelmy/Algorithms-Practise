0ab591f711d6b7d2e3e1244ae7eceb7c7d9ac454
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2023-11-25 00:23:20 +0200

    Revision on: LinearSearch, BinarySearch, TwoCrystalBalls, BubbleSort, Queue and Stack

---
+++ src/day4/TwoCrystalBalls.ts
@@ -0,0 +1,14 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const n = breaks.length;
+    const jump = Math.sqrt(n);
+
+    for (let i = jump; i < n; i += jump) {
+        if (breaks[i]) {
+            for (let j = i - jump; j <= i; j++) {
+                if (breaks[j]) return j;
+            }
+        }
+    }
+
+    return -1;
+}
