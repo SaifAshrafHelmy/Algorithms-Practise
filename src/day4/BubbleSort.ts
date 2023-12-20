97b6a8c1c9b1da26c4a848450ce706cf52db8b5d
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2023-12-20 14:06:37 +0200

    day 5 start

--- src/day4/BubbleSort.ts
+++ src/day4/BubbleSort.ts
@@ -1,6 +1,6 @@
 export default function bubble_sort(arr: number[]): void {
     const n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
+    for (let i = 0; i < n; i++) {
         for (let j = 0; j < n - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 const tmp = arr[j];
