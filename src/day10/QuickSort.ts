0fb108c622f3d06a7e0709cc814b82b4f8c4572e
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2024-02-06 13:14:30 +0200

    Implemented QuickSort + Rev on Path Finding

--- src/day10/QuickSort.ts
+++ src/day10/QuickSort.ts
@@ -1,20 +1,14 @@
 function qs(arr: number[], lo: number, hi: number): void {
     // Base case
-    if (lo >= hi) {
-        return;
-    }
-
+    if (lo >= hi) return;
     // Recursive cases
     const pivotIdx = partition(arr, lo, hi);
     qs(arr, lo, pivotIdx - 1);
     qs(arr, pivotIdx + 1, hi);
 }
-
 function partition(arr: number[], lo: number, hi: number): number {
     const pivot = arr[hi];
-
     let idx = lo - 1;
-
     for (let i = lo; i < hi; i++) {
         if (arr[i] <= pivot) {
             idx++;
@@ -23,6 +17,7 @@ function partition(arr: number[], lo: number, hi: number): number {
             arr[idx] = tmp;
         }
     }
+
     idx++;
     arr[hi] = arr[idx];
     arr[idx] = pivot;
--- src/day9/QuickSort.ts
+++ src/day9/QuickSort.ts
@@ -1,18 +1,14 @@
 function qs(arr: number[], lo: number, hi: number): void {
     // Base case
-    if (lo >= hi) {
-        return;
-    }
+    if (lo >= hi) return;
 
     // Recursive cases
     const pivotIdx = partition(arr, lo, hi);
     qs(arr, lo, pivotIdx - 1);
     qs(arr, pivotIdx + 1, hi);
 }
-
 function partition(arr: number[], lo: number, hi: number): number {
     const pivot = arr[hi];
-
     let idx = lo - 1;
 
     for (let i = lo; i < hi; i++) {
@@ -27,9 +23,11 @@ function partition(arr: number[], lo: number, hi: number): number {
     arr[hi] = arr[idx];
     arr[idx] = pivot;
 
+    // Return pivot index
     return idx;
 }
-
 export default function quick_sort(arr: number[]): void {
+    console.log(arr);
     qs(arr, 0, arr.length - 1);
+    console.log(arr);
 }
